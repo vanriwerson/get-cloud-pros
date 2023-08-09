892e044bf6c38f20b8797859550b98ae17aa644e
Bruno Riwerson Silva <riwerson@gmail.com>
2023-08-09 15:11:51 -0300

    feat: adicionando integrantes

--- script.js
+++ script.js
@@ -2,11 +2,39 @@ const CARDS_CONTAINER = document.getElementById('cards-container');
 
 const cloudPros = [
   {
+    name: 'Adriana Cortes Correa',
+    avatarSrc: 'adriana-cortes-correa.jpeg',
+    resumeHref: 'adriana-cortes-correa.pdf',
+    summary: 'Estou motivada, tendo experiência progressiva em setor de monitoramento. Oferecendo conhecimentos em planilhas de excel, trabalho em equipe, foco e disposição, organização, capaz de navegar por situações de alto estresse e atingir os objetivos.',
+    linkedinUrl: 'https://www.linkedin.com/in/adrianacortescorrea/',
+  },
+  {
     name: 'Bruno Riwerson Silva',
     avatarSrc: 'bruno-riwerson-silva.jpeg',
     resumeHref: 'bruno-riwerson-silva.pdf',
     summary: 'Sou uma pessoa inconformada, automotivada e colaborativa sempre em busca de novos desafios e conhecimentos. Possuo vasta experiência em atendimento ao cliente, focando na geração de valor através da escuta empática e resolução de conflitos.',
     linkedinUrl: 'https://www.linkedin.com/in/bruno-riwerson/',
+  },
+  {
+    name: 'Edmar Marcelino dos Santos',
+    avatarSrc: 'edmar-marcelino-dos-santos.jpg',
+    resumeHref: 'edmar-marcelino-dos-santos.pdf',
+    summary: 'Busco sempre  estar em conexão com as pessoas, sempre  buscando aprender e compartilhar todo apendizado para melhoria contínua. Possuo experiência como Analista de Suporte a Sistemas com expertise em SQL e identificação de causas raiz e tratativas, conhecimento em linguagens de programação.',
+    linkedinUrl: 'https://www.linkedin.com/in/edmar-santos-m/',
+  },
+  {
+    name: 'Leandro Henrique Pereira',
+    avatarSrc: 'leandro-henrique-pereira.jpeg',
+    resumeHref: 'leandro-henrique-pereira.pdf',
+    summary: 'Profissional com experiência de mais de 20 anos na área de telecomunicações com foco em liderança de equipes. Pessoa de fácil convivência, esforçado, dedicado e com fácil adaptação a novos cargos e desafios',
+    linkedinUrl: 'https://www.linkedin.com/in/leandro-henrique-498b05125/',
+  },
+  {
+    name: 'Pedro Henrique Fonseca de Assis',
+    avatarSrc: 'pedro-henrique-fonseca-de-assis.jpeg',
+    resumeHref: 'pedro-henrique-fonseca-de-assis.pdf',
+    summary: 'Estou sempre à disposição para solucionar e aprender com qualquer problema. Tenho uma grande facilidade de lidar em grupo e não perco o controle com facilidade. Meu objetivo é poder ajudar e fazer com que os obstáculos sejam superados e com isso novos conhecimentos sejam adquiridos.',
+    linkedinUrl: 'https://www.linkedin.com/in/pedro-assis-fonseca-693947222/',
   },
 ];
 
